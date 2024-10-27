a24f55f196c8d91e00aac16571eaeae08928b448
Yujun Liu <yujunliu150@gmail.com>
2024-10-27 17:05:17 -0400

    changes

---
+++ code/sb-server/src/main/java/com/example/spring_boot/Application.java
@@ -0,0 +1,19 @@
+package com.example.spring_boot;
+
+import com.example.spring_boot.user.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
